b63f954c82e148f37b4975caf5bc242393c26dc7
balarabetahir <tahirbalarabe2@gmail.com>
2022-09-28 12:59:19 +0100

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,12 @@
+print ( " I know Python and its very easy for everyone " )
+I know Python and its very easy for everyone
+print ( " I know Python and
+its very easy for everyone " )
+# clcoding.com
+  File " C : \ Users \ Irawen \ AppData \ Local \ Temp / ipykernel_13148 / 395849005.py " , line 1
+    print ( " I know Python and
+SyntaxError : EOL while scanning string literal
+#In Python , a backslash ( \ ) is a continuation character .
+print ( " I know Python and \
+its very easy for everyone " )
+# clcoding.com
